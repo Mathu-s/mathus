0bc5957e65483207da7d5e6a5a02d6301f6dc036
Mathu-s <vioxgaming1@gmail.com>
2025-09-04 01:29:28 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -9,32 +9,3 @@ myImage.addEventListener("click", () => {
   }
 });
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `T'es moche ${myName}`;
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `T'es moche ${storedName}`;
-}
-
-myButton.addEventListener("click", () => {
-  setUserName();
-});
-
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `T'es moche ${myName}`;
-  }
-}
