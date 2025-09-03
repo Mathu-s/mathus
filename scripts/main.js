552f093b0a4c4da7bf108b09e805b37fa555926c
Mathu-s <vioxgaming1@gmail.com>
2025-09-04 00:52:48 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/dio-tools.png") {
+    myImage.setAttribute("src", "images/sending-ressources.png");
+  } else {
+    myImage.setAttribute("src", "images/dio-tools.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Ton prénom : ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Ton prénom : ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Ton prénom : ${myName}`;
+  }
+}
